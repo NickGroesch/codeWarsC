78ae17305b5a17ad4e16147a04b9faaf5b59aeaa
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-09-18 10:11:38 -0500

    isIsogram works locally, transform back to cW

---
+++ isIsogram.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) //
+// bool IsIsogram(char *str)
+{ // Code on you crazy diamond!
+    char *str = argv[1];
+    int length = strlen(str);
+    char array[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = 0;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        int arrlen = strlen(array);
+        printf("arrlen=%i at iteration %i \n", arrlen, i);
+        for (int j = 0; j < arrlen; j++)
+        {
+            if (str[i] == array[j])
+            {
+                printf("HEY %c==%c\n", str[i], array[j]);
+                return 1;
+            }
+            printf("match: %c-%c at %i\n", str[i], array[j], j);
+        }
+        array[i] = str[i];
+        printf("%s-%i\n", array, i);
+    }
+    return 0;
+}
+//IsIsogram("abcdefghijklmnop"), 1)
