75f95206f6b06e0841e1cba87b4f2fb8324218dd
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-09-26 18:26:54 -0500

    it took far too long to get those ascii values

---
+++ reverseLetter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    //a=97, z=122, e=101, i=105, o=111, u=117
+    char *str = argv[1];
+
+    // int length = strlen(str);
+    // int index = 0;
+    // for (int i = length - 1; i > -1; i--)
+    // {
+    //     //       if (atoi(str[i]>)
+    // }
+    return 0; //coding and coding..
+}
